355da0baa674d78515d2c0399c8c937bbc1ea6f4
Sebastian Velez Zuluaga <sebastianvelezzuluaga@Sebastians-MacBook-Pro.local>
2022-05-26 21:43:05 -0500

    carda home anda create device

--- components/DevicesScreen.js
+++ components/DevicesScreen.js
@@ -1,15 +1,109 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  Button,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 import NavigationMenu from "./tools/NavigationMenu";
 
 export default function DevicesScreen({ navigation }) {
+  const [name_device, set_name_device] = useState("");
+  const [secret_device, set_secret_device] = useState("");
+  const [machinbe_id, set_machinbe_id] = useState(null);
+  const [loading, set_loading] = useState(false);
+
+  const save_device = () => {
+    console.log("OEOEO")
+    set_loading(true);
+    fetch("https://rksmc4.deta.dev/create_device/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name_device,
+        secret: secret_device,
+        machines_id: machinbe_id,
+      }),
+    }).then((response) => {
+      set_loading(false);
+      if (response.status === 200) {
+        Alert.alert("Se Guardo el  con exito");
+        set_machinbe_id(null);
+        set_name_device("")
+        set_secret_device("")
+      } else {
+        Alert.alert("Error");
+      }
+    });
+  };
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+    >
       <NavigationMenu></NavigationMenu>
-      <Pressable onPress={() => navigation.navigate("home")}>
-        <Text>Dispositivos</Text>
+      <Pressable
+        style={styles.container_form}
+        onPress={() => Keyboard.dismiss()}
+      >
+        <Text style={styles.title_form}>Dispositivos</Text>
+        <View style={styles.row_form}>
+          <Text style={styles.title_form}>Nombre :</Text>
+          <TextInput
+            style={styles.input_form}
+            onChangeText={set_name_device}
+            value={name_device}
+            placeholder={"Nombre del dispositivo"}
+          ></TextInput>
+        </View>
+        <View style={styles.row_form}>
+          <Text style={styles.title_form}>Secret :</Text>
+          <TextInput
+            style={styles.input_form}
+            onChangeText={set_secret_device}
+            value={secret_device}
+            secureTextEntry={true}
+            placeholder={"Secret"}
+          ></TextInput>
+        </View>
+        <View style={styles.row_form}>
+          <Text style={styles.title_form}>id Maquina :</Text>
+          <TextInput
+            style={styles.input_form}
+            onChangeText={set_machinbe_id}
+            value={machinbe_id}
+            keyboardType="numeric"
+          ></TextInput>
+        </View>
+        {/* <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      /> */}
+        <Pressable style={styles.button_form} onPress={save_device}>
+          {loading ? (
+            <ActivityIndicator
+              animating={true}
+              size="large"
+              style={{ opacity: 1 }}
+              color="white"
+            ></ActivityIndicator>
+          ) : (
+            <Text style={styles.button_text_form}>Guardar</Text>
+          )}
+        </Pressable>
       </Pressable>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -19,5 +113,49 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  container_form: {
+    width: 300,
+    height: "50%",
+    backgroundColor: "gray",
+    borderRadius: 20,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  row_form: {
+    width: "95%",
+    height: 30,
+    marginTop: 40,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  title_form: {
+    fontSize: 18,
+    color: "white",
+    width: 100,
+  },
+  input_form: {
+    width: 150,
+    height: "100%",
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+  button_form: {
+    width: "80%",
+    height: 50,
+    backgroundColor: "black",
+    marginTop: 100,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  button_text_form: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
   },
 });
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,14 +1,66 @@
+import React, { useEffect, useRef } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Pressable } from "react-native";
 import NavigationMenu from "./tools/NavigationMenu";
 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 export default function HomeScreen({ navigation }) {
+
+ /*  var mqtt = require("mqtt");
+  var options = {
+    protocol: "mqtts",
+    clientId: "User1",
+    username: "smart",
+    password: "FzSjl27L9Ac9VVlk",
+  };
+
+  const [current_val, setcurrent] = useState("");
+  const [voltage_val, setvoltage_val] = useState("");
+  // Real time graph
+  const [current_graph, setcurrent_graph] = useState([{ id: 0, value: 0 }]);
+
+  let currentref = useRef("");
+  currentref.current = current_val;
+  useEffect(() => {
+    var client = mqtt.connect("mqtt://smart.cloud.shiftr.io", options);
+    client.subscribe("Termofijadora01/fase3/#");
+    var note;
+    client.on("message", function (topic, message) {
+      note = message.toString();
+      if (topic === "Termofijadora01/fase3/corriente") {
+        console.log("corriente", note);
+        cont = cont + 1;
+        setcurrent_graph((current_graph) => [
+          ...current_graph,
+          { id: cont, value: note },
+        ]);
+        setcurrent(note);
+      } else if (topic === "Termofijadora01/fase3/voltaje") {
+        setvoltage_val(note);
+        console.log("voltaje", note);
+      }
+      if (currentref.current === "") {
+        client.end();
+      }
+    });
+  }, []); */
+
   return (
     <View style={styles.container}>
       <NavigationMenu></NavigationMenu>
-      <Pressable onPress={() => navigation.navigate("machines")}>
-        <Text>Home</Text>
-      </Pressable>
+      <View style={styles.card_info}>
+        <Text style={styles.text_info}>Corriente</Text>
+        <View style={styles.line_separator}></View>
+        <View style={styles.row_card}>
+          <MaterialCommunityIcons
+            name="lightning-bolt"
+            size={90}
+            color="white"
+          />
+          <Text style={styles.text_info}>20</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,6 +70,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+  },
+  card_info: {
+    width: "80%",
+    height: 200,
+    backgroundColor: "black",
+    borderRadius: 20,
+    marginTop: 100,
+    display: "flex",
+    alignItems: "center",
+  },
+  text_info: {
+    fontSize: 30,
+    color: "white",
+  },
+  row_card: {
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "center",
+    width: "100%",
+    marginTop: 35,
+  },
+  line_separator: {
+    backgroundColor: "white",
+    height: 2,
+    width: "80%",
   },
 });
