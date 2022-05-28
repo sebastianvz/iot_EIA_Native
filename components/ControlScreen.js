916e6574d43f029a408cc3177c1dbc0b9e5e5ba6
Sebastian Velez Zuluaga <sebastianvelezzuluaga@Sebastians-MacBook-Pro.local>
2022-05-28 09:34:22 -0500

    Obteniendo el estado del mqtt

--- components/ControlScreen.js
+++ components/ControlScreen.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Alert,
+  Animated,
+} from "react-native";
 import NavigationMenu from "./tools/NavigationMenu";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
@@ -14,36 +21,69 @@ var options = {
 
 export default function ControlScreen({ navigation }) {
   const [press, setPress] = useState(false);
+  const [rigthAnimation, setrigthAnimation] = useState(new Animated.Value(0));
 
   const control_led = () => {
     setPress(!press);
+    startAnimation(press ? 0 : 1);
     var client = mqtt.connect("mqtt://iottest45.cloud.shiftr.io", options);
-    client.on("connect", function(){
-        client.subscribe("led", function(err){
-            if(!err){
-                if(press){
-                    client.publish("led","OFF",{qos:0, retain:true})
-                }
-                else{
-                    client.publish("led","ON",{qos:0, retain:true})
-                }
-            }
-        });
+    client.on("connect", function () {
+      client.subscribe("led", function (err) {
+        if (!err) {
+          if (press) {
+            client.publish("led", "OFF", { qos: 0, retain: true });
+          } else {
+            client.publish("led", "ON", { qos: 0, retain: true });
+          }
+        }
+      });
     });
 
-    client.on("message", function(topic, message){
-        console.log(message.toString());
-        client.end()
+    client.on("message", function (topic, message) {
+      console.log(message.toString());
+      client.end();
     });
-  }
+  };
 
+  const startAnimation = (value) => {
+    Animated.timing(rigthAnimation, {
+      toValue: value === 1 ? 75 : -2,
+      duration: 100,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animatedStyle = {
+    right: {
+      transform: [{ translateX: rigthAnimation }],
+    },
+  };
+
+  useEffect(() => {
+    var client = mqtt.connect("mqtt://iottest45.cloud.shiftr.io", options);
+    client.subscribe("led");
+    var note;
+    client.on("message", function (topic, message) {
+      note = message.toString();
+      if (message == "ON") {
+        setPress(true);
+        startAnimation(1);
+      } else {
+        setPress(false);
+        startAnimation(0);
+      }
+      client.end();
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
       <NavigationMenu></NavigationMenu>
       <View style={styles.container_buttons}>
         <Pressable style={styles.button} onPress={control_led}>
-          <View style={press ? styles.circle_rigth : styles.circle}></View>
+          <Animated.View
+            style={[styles.circle, animatedStyle.right]}
+          ></Animated.View>
           <MaterialCommunityIcons
             name="lightbulb-on-outline"
             size={35}
@@ -105,5 +145,5 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
-  }
-})
+  },
+});
